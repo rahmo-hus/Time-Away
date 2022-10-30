2503b6224ec1761d74ddd0d509b8192c062eb826
Rahmo Huseinagic <45545657+rahmo-hus@users.noreply.github.com>
2022-10-30 18:42:11 +0100

    View calendar fixed, as well as some warnings

--- absence-planner/web/src/components/Calendar/Calendar.js
+++ absence-planner/web/src/components/Calendar/Calendar.js
@@ -1,14 +1,17 @@
 import { useAuth } from "@redwoodjs/auth";
 import { useEffect, useState } from 'react'
 import AllowanceBreakdown from "src/components/AllowanceBreakdown";
+import CalendarBody from "src/components/CalendarBody";
 
 const Calendar = ({ leaves, department, allowanceAdjustment, leaveTypes }) => {
 
   const { hasRole } = useAuth();
   const [showFullYear, setShowFullYear] = useState(false);
+  const weekDays = ["Su", "M", "Tu", "W", "Th", "F", "Sa"];
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
   useEffect(() => {
-    console.log(leaveTypes);
+    console.log(leaves)
   });
 
   const calculateDaysTaken = () => {
@@ -31,6 +34,77 @@ const Calendar = ({ leaves, department, allowanceAdjustment, leaveTypes }) => {
     return total;
   }
 
+  const getMonthDays = (year, month) => {
+    let date = 1;
+    const days = [];
+    while (!isNaN(new Date(year + '-' + month + '-' + date).getDate()) && new Date(year + '-' + month + '-' + date).getDate() === date) {
+      days.push(date + '-' + weekDays[new Date(year + '-' + month + '-' + date).getDay()])
+      date += 1;
+    }
+
+    const weeks = [];
+    var week = { Su: null, M: null, Tu: null, W: null, Th: null, F: null, Sa: null };
+
+    for (let i = 0; i < days.length; i++) {
+      const weekDay = days[i].split("-")[1];
+      date = days[i].split("-")[0];
+      week[weekDay] = date;
+      if (weekDay === 'Su') {
+        weeks.push(week);
+        week = { Su: null, M: null, Tu: null, W: null, Th: null, F: null, Sa: null };
+      }
+    }
+
+    if (week.M || week.F || week.Sa || week.W || week.Tu || week.Th || week.Su)
+      weeks.push(week);
+
+    return weeks;
+  }
+
+  const getCalendar = (year, month) => {
+
+    let calendar = {
+      year: year,
+      month: month,
+      weeks: getMonthDays(year, month)
+    };
+
+    return calendar;
+  }
+
+  const getYearsAndMonths = () => {
+    const data = [];
+    let year = new Date().getFullYear();
+    let month = new Date().getMonth();
+
+    if (!showFullYear) {
+      for (let i = 0; i < 4; i++) {
+        if (month === 12) {
+          month = 0;
+          year += 1;
+          data.push({ month: month + 1, year: year });
+        }
+        else {
+          data.push({ month: month + 1, year: year });
+          month++;
+        }
+      }
+    }
+    else {
+      for (let i = 0; i < 12; i++) {
+        if (month === 12) {
+          month = 0;
+          year += 1;
+          data.push({ month: month + 1, year: year });
+        }
+        else {
+          data.push({ month: month + 1, year: year });
+          month++;
+        }
+      }
+    }
+    return data;
+  }
 
   const { isAuthenticated, currentUser } = useAuth();
   return (
@@ -53,13 +127,11 @@ const Calendar = ({ leaves, department, allowanceAdjustment, leaveTypes }) => {
       </div>
 
       <div className="row">
-
         <div className="col-md-2 top-leave-type-statistics">
           <dl>
             <dt data-tom-days-available-in-allowance>{department.allowance - calculateDaysTaken()}</dt>
             <dd>Days available</dd>
             <dd>out of <span data-tom-total-days-in-allowance>{department.allowance}</span> in allowance</dd>
-
           </dl>
         </div>
         <div className="flex-center">
@@ -71,9 +143,14 @@ const Calendar = ({ leaves, department, allowanceAdjustment, leaveTypes }) => {
             <dl>
               <dt>Used so far</dt>
               {
-                leaveTypes.map((leaveType) => (<>
-                  <dd><em> {leaveType.name}</em> <span className="pull-right">{calculateDaysTakenForLeaveTypeName(leaveType.name)} out of {department.allowance}</span></dd>
-                </>))
+                leaveTypes.map((leaveType) => (
+                  <React.Fragment key={leaveType.id}>
+                    <dd>
+                      <em> {leaveType.name}</em>
+                      <span className="pull-right">{calculateDaysTakenForLeaveTypeName(leaveType.name)} out of {department.allowance}</span>
+                    </dd>
+                  </React.Fragment>
+                ))
               }
               {
                 !hasRole('manager') &&
@@ -97,27 +174,25 @@ const Calendar = ({ leaves, department, allowanceAdjustment, leaveTypes }) => {
 
 
       <div className="row main-row_header">
-        <div className="col-md-12">Calendar <a href="/calendar/feeds/" datatoggle="tooltip" dataplacement="right" title="Export absences  to external calendars"/></div>
+        <div className="col-md-12">Calendar <a href="/calendar/feeds/" datatoggle="tooltip" dataplacement="right" title="Export absences  to external calendars" /></div>
       </div>
 
       <div className="row">
         <div className="flex-center">
           <div className="col-xs-2">
-            { showFullYear &&
-      <a className="btn btn-default" ><span aria-hidden="true" className="fa fa-chevron-left"></span> { new Date().getFullYear() - 1} </a>
-      }
+            {showFullYear &&
+              <a className="btn btn-default" ><span aria-hidden="true" className="fa fa-chevron-left"></span> {new Date().getFullYear() - 1} </a>
+            }
           </div>
           <div className="col-xs-8 calendar-section-caption">
 
-            { showFullYear ? <strong>January - December {new Date().getFullYear()}</strong>: <strong>Upcoming Months</strong> }
+            {showFullYear ? <strong>January - December {new Date().getFullYear()}</strong> : <strong>Upcoming Months</strong>}
             &nbsp;
-
-            {/* {{# unless show_full_year }}
-      <a className="btn btn-default" href="/calendar/?show_full_year=1">More... &nbsp;<span className="fa fa-plus"></span></a>
-      {{ else}}
-      <a className="btn btn-default" href="/calendar/">Less... &nbsp;<span className="fa fa-minus"></span></a>
-      {{/ unless}} */}
-
+            {!showFullYear ?
+              <button className="btn btn-default" onClick={() => setShowFullYear(true)}>More... &nbsp;<span className="fa fa-plus"></span></button>
+              :
+              <button className="btn btn-default" onClick={() => setShowFullYear(false)}>Less... &nbsp;<span className="fa fa-minus"></span></button>
+            }
 
           </div>
           <div className="col-xs-2">
@@ -130,43 +205,55 @@ const Calendar = ({ leaves, department, allowanceAdjustment, leaveTypes }) => {
         <div className="row">&nbsp;</div>
         <div className="row clearfix">
 
-          {/* {{# each calendar }} */}
-          <div className="col-md-3 month_container">
-            <table className="calendar_month month_{{ this.month }}">
-              <thead>
-                <tr>
-                  <td colSpan="14">Date</td>
-                </tr>
-                <tr>
-                  <td colSpan="2">M</td>
-                  <td colSpan="2">T</td>
-                  <td colSpan="2">W</td>
-                  <td colSpan="2">T</td>
-                  <td colSpan="2">F</td>
-                  <td colSpan="2">S</td>
-                  <td colSpan="2">S</td>
-                </tr>
-              </thead>
-              <tbody>
-                {/* {{ #each this.weeks }} */}
-                <tr>
-                  {/* {{ #each this }}
-            {{> calendar_cell day = this}}
-            {{/ each}} */}
-                </tr>
-                {/* {{/ each}} */}
-
-              </tbody></table>
-          </div>
-          {/* {{/ each}} */}
+          {
+            getYearsAndMonths().map((data, key) => (
+              <React.Fragment key={key}>
+                <div className="col-md-3 month_container">
+                  <table className={"calendar_month month_" + months[data.month - 1]}>
+                    <thead>
+                      <tr>
+                        <td colSpan="14">{months[data.month - 1]}, {data.year}</td>
+                      </tr>
+                      <tr>
+                        <td colSpan="2">M</td>
+                        <td colSpan="2">T</td>
+                        <td colSpan="2">W</td>
+                        <td colSpan="2">T</td>
+                        <td colSpan="2">F</td>
+                        <td colSpan="2">S</td>
+                        <td colSpan="2">S</td>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {
+                        getCalendar(data.year, data.month).weeks.map((week, key) => (
+                          <React.Fragment key={key}>
+                            <tr>
+                              <CalendarBody data={{ day: week.M, weekend: false }}></CalendarBody>
+                              <CalendarBody data={{ day: week.Tu, weekend: false }}></CalendarBody>
+                              <CalendarBody data={{ day: week.W, weekend: false }}></CalendarBody>
+                              <CalendarBody data={{ day: week.Th, weekend: false }}></CalendarBody>
+                              <CalendarBody data={{ day: week.F, weekend: false }}></CalendarBody>
+                              <CalendarBody data={{ day: week.Sa, weekend: true }}></CalendarBody>
+                              <CalendarBody data={{ day: week.Su, weekend: true }}></CalendarBody>
+                            </tr>
+                          </React.Fragment>
+                        ))
+                      }
+                    </tbody></table>
+                </div>
+              </React.Fragment>
+            ))
+          }
         </div>
       </div>
 
-      {/* {{# unless show_full_year }} */}
-
-      <div className="row main-row_header">
-        <p className="col-md-12">All my absences in 2022</p>
-      </div>
+      {showFullYear
+        &&
+        <div className="row main-row_header">
+          <p className="col-md-12">All my absences in 2022</p>
+        </div>
+      }
 
 
     </>
